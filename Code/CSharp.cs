ada1ee5b67f8527a0ab6a88d48653ad38cbd82a1
helioncode <elioncode99@gmail.com>
2023-10-04 16:43:25 +0700

    update

---
+++ Code/CSharp.cs
@@ -0,0 +1,13 @@
+using System;//namespace
+
+#region Class
+class Person {
+    //deklaration
+    int variable = "integer";
+    string varString = "Text";
+}
+#endregion
+//enum is kelompok constan data
+enum Day{
+
+}
